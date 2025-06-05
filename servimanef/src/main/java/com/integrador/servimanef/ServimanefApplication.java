0f02256805da7f6445ff49a3bfb50473d942ae99
yosriel <96359361+YosRiel@users.noreply.github.com>
2025-06-04 20:57:00 -0500

    springboot

---
+++ servimanef/src/main/java/com/integrador/servimanef/ServimanefApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.servimanef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServimanefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServimanefApplication.class, args);
+	}
+
+}
